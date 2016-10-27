8ae9fb937519586abe1418eae5ecb22ac92e22b7
Pascal Seitz <pascal.seitz@sap.com>
2016-10-27 17:20:25 +0200

    oh yeah

---
+++ randomaccess.js
@@ -0,0 +1,75 @@
+'use strict'
+let msgpack = require("msgpack-lite");
+let fs = require("fs");
+let Promise = require("bluebird")
+
+let useMsgPack = true
+
+function getObject(file, offset, length, cb){
+    let fd = fs.openSync('random.data', 'r')
+    let buf = Buffer(length)
+    fs.read(fd, buf, 0, length, offset, (err, bytesRead, buffer) => {
+        let data = useMsgPack ? msgpack.decode(buffer) : JSON.parse(buffer.toString())
+        fs.closeSync(fd)
+        cb(err, data)
+    })
+}
+getObject = Promise.promisify(getObject);
+
+function getObjectAtPos(file, pos, offsets){
+    return getObject(file, offsets[pos], offsets[pos+1] - offsets[pos])
+}
+
+function getObjectsAtPos(file, positions, offsets){
+    return Promise.map(positions, pos => getObjectAtPos(file, pos, offsets))
+}
+
+function writeArray(filename, arr){
+    let offsets = []
+
+    let fd = fs.openSync(filename, 'w')
+    let currentOffset = 0;
+    for (let entry of arr)
+    {
+        let pack =  useMsgPack ? msgpack.encode(entry) : (new Buffer(JSON.stringify(entry)))
+        fs.writeSync(fd, pack, 0, pack.length, currentOffset)
+        offsets.push(currentOffset)
+        currentOffset += pack.length
+    }
+    offsets.push(currentOffset)
+    fs.closeSync(fd)
+
+    console.log(offsets[1000]);
+    fs.writeFileSync(filename+'.offsets', new Buffer(new Uint32Array(offsets).buffer))
+
+}
+
+// console.time("jmdict.json")
+// let data = require("./jmdict.json")
+// console.timeEnd("jmdict.json")
+// writeArray('random.data', data)
+
+
+console.time("random.data.offsets")
+let offsets = new Uint32Array(fs.readFileSync("random.data.offsets").buffer)
+console.timeEnd("random.data.offsets")
+
+// console.log(offsets[1000]);
+
+console.time("readall")
+getObjectsAtPos('random.data', [350, 500, 265, 255, 980, 350, 500, 265, 255, 980], offsets).then(data => {
+    console.timeEnd("readall")
+    // console.log(data);
+})
+
+
+// getObjectAtPos('random.data', 350, offsets).then((data) => {    
+//     console.log(data);
+// })
+
+
+let service = {}
+service.writeArray = writeArray
+service.getObjectAtPos = getObjectAtPos
+service.getObjectsAtPos = getObjectsAtPos
+
